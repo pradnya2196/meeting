a4846f29d6f400ff6707af6f4d33e5933a5592dd
pradnya2196 <pgole21@gmail.com>
2018-01-30 16:12:29 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Meeting
+
+Manages meetings and schedules
+
+#### License
+
+MIT
---
+++ meeting/api.py
@@ -0,0 +1,67 @@
+#from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import nowdate, add_days
+
+
+@frappe.whitelist()
+def send_invitation_emails(meeting):
+	meeting = frappe.get_doc("Meeting", meeting)
+	meeting.check_permission("email")
+
+	if meeting.status == "Planned":
+		frappe.sendmail(
+			recipients=[d.attendee for d in meeting.attendee],
+			sender=frappe.session.user,
+			subject=meeting.title,
+			message=meeting.invitation_message,
+			reference_doctype=meeting.doctype,
+			reference_name=meeting.name,
+			# as_bulk=True
+		)
+
+
+		meeting.status="Invitation Sent"
+		meeting.save()
+
+		frappe.msgprint(_("Invitation Sent"))
+
+	else:
+		frappe.msgprint(_("Meeting Status must be 'Planned'"))
+
+@frappe.whitelist()
+def get_meetings(start, end):
+	if not frappe.has_permission("Meeting", "read"):
+		raise frappe.PermissionError
+
+	return frappe.db.sql("""select
+		timestamp(`date`, from_time) as start,
+		timestamp(`date`, to_time) as end,
+		name,
+		title,
+		status,
+		0 as all_day
+	from `tabMeeting`
+	where `date` between %(start)s and %(end)s""", {
+		"start": start,
+		"end": end
+	}, as_dict=True)
+
+@frappe.whitelist()
+def make_orientation_meeting(doc, method):
+	meeting = frappe.get_doc({
+		"doctype": "Meeting",
+		"title": "Orientation for {0}".format(doc.first_name),
+		"date": add_days(nowdate(), 1),
+		"from_time": "09:00",
+		"to_time": "09:30",
+		"status": "Planned",
+		"attendees": [{
+			"attendee": doc.name
+		}]
+	})
+
+	meeting.flags.ignore_permissions = True
+	meeting.insert()
+
+	frappe.msgprint(_("Orientation meeting created"))
